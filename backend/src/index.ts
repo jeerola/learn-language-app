544ded3bdbb4ac67cd01f4f578a10a11095b2057
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-03-04 15:32:45 +0200

    feat(index.ts): add graceful shutdown to express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,11 +8,22 @@ const app = express();
 // Fallback to 3000 if PORT is not set in .env
 const port = process.env.PORT || 3000;
 
-
 app.get("/", (req, res) => {
   res.status(200).send("Hello world!");
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+const gracefulShutdown = () => {
+  console.log("Shutting down gracefully...");
+  server.close(() => {
+    console.log("Server closed gracefully.");
+    // database.end(); // TODO: Implement database
+    process.exit(0);
+  });
+};
+
+process.on("SIGINT", gracefulShutdown); // Used in local development (CTRL+C)
+process.on("SIGTERM", gracefulShutdown); // Used in cloud development
