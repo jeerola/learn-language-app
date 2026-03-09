0c236e15e8ac1caab64e9a58822a2c96ffa97e16
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-03-09 23:04:29 +0200

    feat(types): add WordPair and Language types

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,13 @@
+export type Language = {
+  id: number;
+  name: string;
+  code: string;
+};
+
+export type WordPair = {
+    id: number;
+    word1: string;
+    language1: string;
+    word2: string;
+    language2: string;
+};
