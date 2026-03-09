75c3e2fc482f3c6427d44003bbfcf2ce4e0fbf92
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-03-09 13:59:16 +0200

    feat(index.ts): register word and language routers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
 
 import pool from "./db/pool";
+import languageRouter from "./routes/languageRouter";
+import wordRouter from "./routes/wordRouter";
 
 const app = express();
 
 // Fallback to 3000 if PORT is not set in .env
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/api/languages", languageRouter);
+app.use("/api/words", wordRouter);
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello world!");
