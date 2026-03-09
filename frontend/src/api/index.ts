768fec25d7924029d130a11a39badb696f5528a9
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-03-09 23:20:25 +0200

    feat(api): add getWordPairs and getLanguages functions
    Functions fetch data from backend API and return typed arrays
    for use in components.

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,15 @@
+import type { Language, WordPair } from "../types";
+
+export async function getLanguages() {
+  const response = await fetch("/api/languages");
+  const data = await response.json() as Language[];
+
+  return data;
+}
+
+export async function getWordPairs() {
+  const response = await fetch("/api/words");
+  const data = await response.json() as WordPair[];
+
+  return data;
+}
