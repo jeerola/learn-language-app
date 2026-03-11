62ef80ee2376083ecd1d7aa0fdfe7621a9b7a2fa
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-03-12 00:43:29 +0200

    feat(api): add deleteWordPair function, minor refactoring on existing functions

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,14 +2,20 @@ import type { Language, WordPair } from "../types";
 
 export async function getLanguages() {
   const response = await fetch("/api/languages");
-  const data = await response.json() as Language[];
+  const data = (await response.json()) as Language[];
 
   return data;
 }
 
 export async function getWordPairs() {
   const response = await fetch("/api/words");
-  const data = await response.json() as WordPair[];
+  const data = (await response.json()) as WordPair[];
 
   return data;
+}
+
+export async function deleteWordPair(id: number) {
+  await fetch(`/api/words/${id}`, {
+    method: "DELETE",
+  });
 }
