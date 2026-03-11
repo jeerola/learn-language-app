792d93a5f7472ffe01fc867150dd3fe72a5e0069
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-03-12 00:38:42 +0200

    feat(App): delete temporary testing, add AdminPage view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,11 @@
-import { useState } from "react";
 import "./App.css";
 
-import { getLanguages, getWordPairs } from "./api";
-import type { Language, WordPair } from "./types";
+import { AdminPage } from "./pages/AdminPage";
 
 function App() {
-  const [languages, setLanguages] = useState<Language[]>([]);
-  const [wordPairs, setWordPairs] = useState<WordPair[]>([]);
-
   return (
-    <div>
-      <button
-        onClick={async () => {
-          const data = await getLanguages();
-          setLanguages(data);
-          console.log(data);
-        }}
-      >
-        Fetch Languages
-      </button>
-      <ul>
-        {languages.map((language) => (
-          <li key={language.id}>{language.name}</li>
-        ))}
-      </ul>
-
-      <button
-        onClick={async () => {
-          const data = await getWordPairs();
-          setWordPairs(data);
-          console.log(data);
-        }}
-      >
-        Fetch Word Pairs
-      </button>
-      <ul>
-        {wordPairs.map((wordPair) => (
-          <li key={wordPair.id}>
-            {wordPair.word1} - {wordPair.word2}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    <AdminPage/>
+  )
 }
 
 export default App;
