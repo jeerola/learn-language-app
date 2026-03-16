3ec011ad4ec4b68cde53af0ad933abf1601edf05
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-03-16 14:27:43 +0200

    feat(App): add view toggle between admin and user

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 import { AdminPage } from "./pages/AdminPage";
+import { UserPage } from "./pages/UserPage";
 
 function App() {
+  const [currentView, setCurrentView] = useState("admin");
+
   return (
-    <AdminPage/>
-  )
+    <div>
+      <button onClick={() => setCurrentView("admin")}>Admin View</button>
+
+      <button onClick={() => setCurrentView("user")}>User View</button>
+      {currentView === "admin" && <AdminPage />}
+      {currentView === "user" && <UserPage />}
+    </div>
+  );
 }
 
 export default App;
