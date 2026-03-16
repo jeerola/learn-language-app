4c3ab96c50855a48e928e96202a5eed6b88c4ec8
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-03-16 12:49:16 +0200

    feat(AdminPage): add WordPairForm to AdminPage

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -1,31 +1,49 @@
 import { useEffect, useState } from "react";
 
-import { deleteWordPair, getWordPairs } from "../api";
-import type { WordPair } from "../types";
+import { deleteWordPair, getLanguages, getWordPairs } from "../api";
+import { type Language, type WordPair } from "../types";
+import { WordPairForm } from "../components/WordPairForm";
 
 export const AdminPage = () => {
   const [wordPairs, setWordPairs] = useState<WordPair[]>([]);
+  const [languages, setLanguages] = useState<Language[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await getWordPairs();
-      setWordPairs(data);
+      const wordPairData = await getWordPairs();
+      const languagesData = await getLanguages();
+
+      setWordPairs(wordPairData);
+      setLanguages(languagesData);
     };
     fetchData();
   }, []);
 
   const handleDelete = async (id: number) => {
     await deleteWordPair(id);
-    setWordPairs(current => current.filter(wp => wp.id !== id));
-  }
+    setWordPairs((current) => current.filter((wp) => wp.id !== id));
+  };
+
+  const handleWordPairCreated = (newPair: WordPair) => {
+    setWordPairs((current) => [...current, newPair]);
+  };
+
   return (
     <div>
       <h1>Admin Page</h1>
+      {languages.length > 0 && (
+        <WordPairForm
+          languages={languages}
+          defaultLanguageId={languages[0].id}
+          onWordPairCreated={handleWordPairCreated}
+        />
+      )}
 
       <ul>
         {wordPairs.map((wordPair) => (
           <li key={wordPair.id}>
-            {wordPair.word1} - {wordPair.word2} <button onClick={() => handleDelete(wordPair.id)}>Delete</button>
+            {wordPair.word1} - {wordPair.word2}
+            <button onClick={() => handleDelete(wordPair.id)}>Delete</button>
           </li>
         ))}
       </ul>
