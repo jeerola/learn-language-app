1f62705384db86af648384138bf9588570fc53cb
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-03-23 12:18:41 +0200

    feat(WordPairForm): add chakra ui components to form

--- frontend/src/components/WordPairForm.tsx
+++ frontend/src/components/WordPairForm.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import type { Language, WordPair } from "../types";
 import { createWordPair, updateWordPair } from "../api";
 
+import { Button, HStack, Input, NativeSelect, VStack } from "@chakra-ui/react";
 interface Props {
   editingPair: WordPair | null;
   languages: Language[];
@@ -10,7 +11,13 @@ interface Props {
   onWordPairUpdated: (wordPair: WordPair) => void;
 }
 
-export function WordPairForm({ languages, defaultLanguageId, editingPair, onWordPairCreated, onWordPairUpdated}: Props) {
+export function WordPairForm({
+  languages,
+  defaultLanguageId,
+  editingPair,
+  onWordPairCreated,
+  onWordPairUpdated,
+}: Props) {
   const [word1, setWord1] = useState(editingPair ? editingPair.word1 : "");
   const [word2, setWord2] = useState(editingPair ? editingPair.word2 : "");
   const [languageId1, setLanguageId1] = useState(defaultLanguageId);
@@ -18,11 +25,7 @@ export function WordPairForm({ languages, defaultLanguageId, editingPair, onWord
 
   const handleSubmit = async () => {
     if (editingPair != null) {
-      const updatedPair = await updateWordPair(
-        editingPair.id,
-        word1,
-        word2
-      );
+      const updatedPair = await updateWordPair(editingPair.id, word1, word2);
       onWordPairUpdated(updatedPair);
     } else {
       const newWordPair = await createWordPair(
@@ -33,45 +36,61 @@ export function WordPairForm({ languages, defaultLanguageId, editingPair, onWord
       );
       onWordPairCreated(newWordPair);
     }
-  }
+  };
 
   return (
-    <div>
+    <VStack>
       <form action="">
-        <input
-          type="text"
-          value={word1}
-          onChange={(e) => setWord1(e.target.value)}
-        />
-        <input
-          type="text"
-          value={word2}
-          onChange={(e) => setWord2(e.target.value)}
-        />
-        <fieldset>
-          <select
-            value={languageId1}
-            onChange={(e) => setLanguageId1(parseInt(e.target.value))}
-          >
-            {languages.map((language) => (
-              <option key={language.id} value={language.id}>
-                {language.name}
-              </option>
-            ))}
-          </select>
-          <select
-            value={languageId2}
-            onChange={(e) => setLanguageId2(parseInt(e.target.value))}
-          >
-            {languages.map((language) => (
-              <option key={language.id} value={language.id}>
-                {language.name}
-              </option>
-            ))}
-          </select>
-        </fieldset>
+        <HStack>
+          <Input
+            placeholder="Word 1"
+            type="text"
+            value={word1}
+            onChange={(e) => setWord1(e.target.value)}
+          />
+
+          <NativeSelect.Root>
+            <NativeSelect.Field
+              value={languageId1}
+              onChange={(e) => setLanguageId1(parseInt(e.target.value))}
+            >
+              {languages.map((language) => (
+                <option key={language.id} value={language.id}>
+                  {language.name}
+                </option>
+              ))}
+            </NativeSelect.Field>
+          </NativeSelect.Root>
+
+          <Input
+            placeholder="Word 2"
+            type="text"
+            value={word2}
+            onChange={(e) => setWord2(e.target.value)}
+          />
+
+          <NativeSelect.Root>
+            <NativeSelect.Field
+              value={languageId2}
+              onChange={(e) => setLanguageId2(parseInt(e.target.value))}
+            >
+              {languages.map((language) => (
+                <option key={language.id} value={language.id}>
+                  {language.name}
+                </option>
+              ))}
+            </NativeSelect.Field>
+          </NativeSelect.Root>
+        </HStack>
       </form>
-      <button onClick={() => handleSubmit()}> {editingPair ? "Edit word pair" : "Add word pair"} </button>
-    </div>
+
+      <Button
+        variant={"outline"}
+        colorPalette={ editingPair ? "blue" : "green" }
+        onClick={() => handleSubmit()}
+      >
+        {editingPair ? "Edit word pair" : "Add word pair"}
+      </Button>
+    </VStack>
   );
 }
