61ce0b1bcf6e8250f6087bcfbbee2d6e27213e86
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-03-23 12:02:11 +0200

    feat(AdminPage): add chakra ui components and table view of word pairs

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { deleteWordPair, getLanguages, getWordPairs } from "../api";
 import { type Language, type WordPair } from "../types";
 import { WordPairForm } from "../components/WordPairForm";
+import { VStack, Heading, Table, Button, Stack } from "@chakra-ui/react";
 
 export const AdminPage = () => {
   const [wordPairs, setWordPairs] = useState<WordPair[]>([]);
@@ -31,14 +32,16 @@ export const AdminPage = () => {
 
   const handleWordPairUpdated = (updatedWordPair: WordPair) => {
     setWordPairs((current) =>
-      current.map((wp) => (wp.id === updatedWordPair.id ? updatedWordPair : wp)),
+      current.map((wp) =>
+        wp.id === updatedWordPair.id ? updatedWordPair : wp,
+      ),
     );
     setEditingPair(null);
-  }
+  };
 
   return (
-    <div>
-      <h1>Admin Page</h1>
+    <VStack>
+      <Heading size={"3xl"}>Admin Page</Heading>{" "}
       {languages.length > 0 && (
         <WordPairForm
           editingPair={editingPair}
@@ -48,16 +51,46 @@ export const AdminPage = () => {
           onWordPairUpdated={handleWordPairUpdated}
         />
       )}
-
-      <ul>
-        {wordPairs.map((wordPair) => (
-          <li key={wordPair.id}>
-            {wordPair.word1} - {wordPair.word2}
-            <button onClick={() => setEditingPair(wordPair)}>EDIT</button>
-            <button onClick={() => handleDelete(wordPair.id)}>DELETE</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+      <Stack>
+        <Heading>Word pairs</Heading>
+        <Table.Root>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeader>Word 1</Table.ColumnHeader>
+              <Table.ColumnHeader>Language 1</Table.ColumnHeader>
+              <Table.ColumnHeader>Word 2</Table.ColumnHeader>
+              <Table.ColumnHeader>Language 2</Table.ColumnHeader>
+              <Table.ColumnHeader>Actions</Table.ColumnHeader>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {wordPairs.map((wordPair) => (
+              <Table.Row key={wordPair.id}>
+                <Table.Cell>{wordPair.word1}</Table.Cell>
+                <Table.Cell>{wordPair.language1}</Table.Cell>
+                <Table.Cell>{wordPair.word2}</Table.Cell>
+                <Table.Cell>{wordPair.language2}</Table.Cell>
+                <Table.Cell>
+                  <Button
+                    variant={"outline"}
+                    colorPalette={"yellow"}
+                    onClick={() => setEditingPair(wordPair)}
+                  >
+                    EDIT
+                  </Button>
+                  <Button
+                    variant={"outline"}
+                    colorPalette={"red"}
+                    onClick={() => handleDelete(wordPair.id)}
+                  >
+                    DELETE
+                  </Button>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      </Stack>
+    </VStack>
   );
 };
