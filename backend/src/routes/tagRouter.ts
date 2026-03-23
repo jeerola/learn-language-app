59dfe1ce970f54ed204dbdcc91eb55a90c032df7
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-03-23 23:42:45 +0200

    feat(tagRouter): add DELETE method

--- backend/src/routes/tagRouter.ts
+++ backend/src/routes/tagRouter.ts
@@ -39,7 +39,33 @@ router.post("/", async (req, res) => {
       console.error("Error creating tag: ", error);
       res.status(500).json({ error: "Internal server error" });
     }
-
 })
+
+/**
+ * Deletes a tag from database
+ */
+router.delete("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  // Reject request if ID is not a positive integer
+  if (isNaN(id) || id <= 0) {
+    res.status(400).json({ error: "Invalid ID" });
+    return;
+  }
+
+  try {
+    const result = await pool.query("DELETE FROM tags WHERE id = $1", [
+      id,
+    ]);
+    if (result.rowCount === 0) {
+      res.status(404).json({ error: "Tag not found." });
+      return;
+    }
+    res.status(204).send();
+  } catch (error) {
+    console.error("Error deleting a tag: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 export default router;
