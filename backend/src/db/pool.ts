95c588049a74a0141fd7cd65994cf250e4789664
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-03-24 11:59:13 +0200

    feat(pool): add DATABASE_URL variable check for cloud deployment

--- backend/src/db/pool.ts
+++ backend/src/db/pool.ts
@@ -3,12 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? { connectionString: process.env.DATABASE_URL }
+    : {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+      },
+);
 
 export default pool;
