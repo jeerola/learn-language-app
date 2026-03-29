083f3e92dddfb41bd8051afed6f79d796ee9e6ad
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-03-29 13:49:17 +0300

    feat(UserPage): improve User UI feedback and randomize words

--- frontend/src/pages/userView/UserPage.tsx
+++ frontend/src/pages/userView/UserPage.tsx
@@ -1,5 +1,13 @@
 import { useState, useEffect } from "react";
-import { Card, Heading, Input, Group, Button, VStack } from "@chakra-ui/react";
+import {
+  Card,
+  Heading,
+  Input,
+  Group,
+  Button,
+  VStack,
+  Text,
+} from "@chakra-ui/react";
 
 import { type WordPair } from "../../types";
 import { getWordPairs } from "../../api";
@@ -9,6 +17,9 @@ export const UserPage = () => {
   const [score, setScore] = useState(0);
   const [userInput, setUserInput] = useState("");
   const [currentWordPair, setCurrentWordPair] = useState(0);
+  const [previousAnswerCorrect, setPreviousAnswerCorrect] = useState<
+    boolean | null
+  >(null);
 
   const handleSubmit = () => {
     const isCorrect =
@@ -27,12 +38,13 @@ export const UserPage = () => {
       alert(`You scored ${finalScore} out of ${wordPairs.length} points!`);
     }
     setUserInput("");
+    setPreviousAnswerCorrect(isCorrect);
   };
 
   useEffect(() => {
     const fetchData = async () => {
       const wordPairData = await getWordPairs();
-
+      wordPairData.sort(() => Math.random() - 0.5); // randomize word pairs order
       setWordPairs(wordPairData);
     };
     fetchData();
@@ -40,10 +52,12 @@ export const UserPage = () => {
 
   return (
     <VStack mx={"auto"} gap={"4"}>
-      <Heading size={"3xl"}>User Page</Heading>
+      <Heading size={"3xl"}>Practice!</Heading>
       <Card.Root w={"sm"} maxW={"sm"} mx={"auto"}>
         <Card.Header textAlign="center">
           Word: {currentWordPair + 1} / {wordPairs.length}
+          <br />
+          Correct: {score} / {wordPairs.length}
         </Card.Header>
         <Card.Body textAlign="center">
           {wordPairs.length > 0 && <p>{wordPairs[currentWordPair]?.word1}</p>}
@@ -68,6 +82,11 @@ export const UserPage = () => {
           Submit
         </Button>
       </Group>
+      {previousAnswerCorrect === null ? null : (
+        <Text color={previousAnswerCorrect ? "green" : "red"}>
+          {previousAnswerCorrect ? "✅ Correct!" : " ❌ Wrong!"}
+        </Text>
+      )}
     </VStack>
   );
 };
