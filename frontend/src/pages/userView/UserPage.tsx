2f661ab13d57c287396fe68994699f4772657ad8
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-04-09 14:04:03 +0300

    feat(UserPage): add tag filtering for word pairs

--- frontend/src/pages/userView/UserPage.tsx
+++ frontend/src/pages/userView/UserPage.tsx
@@ -8,9 +8,9 @@ import {
   VStack,
   Text,
 } from "@chakra-ui/react";
-
-import { type WordPair } from "../../types";
+import { type WordPair, type Tag } from "../../types";
 import { getWordPairs } from "../../api";
+import { UserLanding } from "./UserLanding";
 
 export const UserPage = () => {
   const [wordPairs, setWordPairs] = useState<WordPair[]>([]);
@@ -20,6 +20,22 @@ export const UserPage = () => {
   const [previousAnswerCorrect, setPreviousAnswerCorrect] = useState<
     boolean | null
   >(null);
+  const [selectedTag, setSelectedTag] = useState<Tag | null>(null);
+  const [view, setView] = useState<"landing" | "practice">("landing");
+
+  const handleTagSelect = (tag: Tag | null) => {
+    setSelectedTag(tag);
+    setView("practice");
+  };
+
+  const handleBack = () => {
+    setWordPairs([])
+    setUserInput("")
+    setScore(0);
+    setCurrentWordPair(0);
+    setSelectedTag(null);
+    setView("landing");
+  }
 
   const handleSubmit = () => {
     const isCorrect =
@@ -45,48 +61,75 @@ export const UserPage = () => {
     const fetchData = async () => {
       const wordPairData = await getWordPairs();
       wordPairData.sort(() => Math.random() - 0.5); // randomize word pairs order
-      setWordPairs(wordPairData);
+
+      if (selectedTag === null) {
+        setWordPairs(wordPairData);
+      } else {
+        const filteredWords = [];
+        for (const wordPair of wordPairData) {
+          for (const tag of wordPair.tags) {
+            if (tag.id === selectedTag.id) {
+              filteredWords.push(wordPair);
+            }
+          }
+        }
+        setWordPairs(filteredWords);
+      }
     };
     fetchData();
-  }, []);
+  }, [selectedTag]);
 
   return (
-    <VStack mx={"auto"} gap={"4"}>
-      <Heading size={"3xl"}>Practice!</Heading>
-      <Card.Root w={"sm"} maxW={"sm"} mx={"auto"}>
-        <Card.Header textAlign="center">
-          Word: {currentWordPair + 1} / {wordPairs.length}
-          <br />
-          Correct: {score} / {wordPairs.length}
-        </Card.Header>
-        <Card.Body textAlign="center">
-          {wordPairs.length > 0 && <p>{wordPairs[currentWordPair]?.word1}</p>}
-        </Card.Body>
-      </Card.Root>
+    <>
+      {view === "landing" && <UserLanding onTagSelect={handleTagSelect} />}
+      {view === "practice" && (
+        <VStack mx={"auto"} gap={"4"}>
+          <Heading size={"3xl"}>Practice!</Heading>
+          <Card.Root w={"sm"} maxW={"sm"} mx={"auto"}>
+            <Card.Header textAlign="center">
+              Word: {currentWordPair + 1} / {wordPairs.length}
+              <br />
+              Correct: {score} / {wordPairs.length}
+            </Card.Header>
+            <Card.Body textAlign="center">
+              {wordPairs.length > 0 && (
+                <p>{wordPairs[currentWordPair]?.word1}</p>
+              )}
+            </Card.Body>
+          </Card.Root>
 
-      <Group w={"sm"}>
-        <Input
-          variant={"outline"}
-          type="text"
-          value={userInput}
-          onChange={(e) => setUserInput(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") handleSubmit();
-          }}
-        />
-        <Button
-          variant={"outline"}
-          colorPalette={"yellow"}
-          onClick={() => handleSubmit()}
-        >
-          Submit
-        </Button>
-      </Group>
-      {previousAnswerCorrect === null ? null : (
-        <Text color={previousAnswerCorrect ? "green" : "red"}>
-          {previousAnswerCorrect ? "✅ Correct!" : " ❌ Wrong!"}
-        </Text>
+          <Group w={"sm"}>
+            <Input
+              variant={"outline"}
+              type="text"
+              value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") handleSubmit();
+              }}
+            />
+            <Button
+              variant={"outline"}
+              colorPalette={"yellow"}
+              onClick={() => handleSubmit()}
+            >
+              Submit
+            </Button>
+          </Group>
+          {previousAnswerCorrect === null ? null : (
+            <Text color={previousAnswerCorrect ? "green" : "red"}>
+              {previousAnswerCorrect ? "✅ Correct!" : " ❌ Wrong!"}
+            </Text>
+          )}
+          <Button
+            variant={"outline"}
+            colorPalette={"yellow"}
+            onClick={() => handleBack()}
+          >
+            Back
+          </Button>
+        </VStack>
       )}
-    </VStack>
+    </>
   );
 };
